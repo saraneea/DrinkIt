b99d5c72b044b434495628e52e7ce6fae9e894c0
siiseli19 <sami.ojala97@gmail.com>
2021-04-28 15:59:29 +0300

    k

--- Drinkkiohjeet.php
+++ Drinkkiohjeet.php
@@ -5,3 +5,11 @@
 <h2> moroooo000 </h2>
 <h3> uuuuuiiiiii </h3>
 <p style="color: blue">homo.</p>
+
+
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?>
