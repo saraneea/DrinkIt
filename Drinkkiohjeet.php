db2f0946394329cc8fb54dad45f5c27310b1d74f
siiseli19 <sami.ojala97@gmail.com>
2021-04-28 16:05:14 +0300

    muutos

--- Drinkkiohjeet.php
+++ Drinkkiohjeet.php
@@ -1,15 +1,3 @@
-</body>
-</html>
+$tervehdys = "no morooo";
 
-<h1>perkeleeee</h1>
-<h2> moroooo000 </h2>
-<h3> uuuuuiiiiii </h3>
-<p style="color: blue">homo.</p>
-
-
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
-?>
+echo <h2> $tervehdys <h2>
