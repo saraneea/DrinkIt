cba8f1259764154d4d2e27de6ab53e6b3e998008
siiseli19 <sami.ojala97@gmail.com>
2021-05-09 22:59:54 +0300

    scrollaus

--- Drinkkiohjeet.php
+++ Drinkkiohjeet.php
@@ -1,15 +1,41 @@
 <!DOCTYPE html>
 <html>
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script>
-
-</script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
+
+
 #section1 {
   height: 600px;
   background-color: grey;
 }
+
+
+
+
+
+
+
+
 
 </style>
 </head>
