db9e7fa910d519ba75a0035ef52bece7c4753913
siiseli19 <sami.ojala97@gmail.com>
2021-05-09 22:47:54 +0300

    korjaus

--- Drinkkiohjeet.php
+++ Drinkkiohjeet.php
@@ -1,32 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
 </script>
 <style>
 #section1 {
@@ -42,18 +17,16 @@ $(document).ready(function(){
 </head>
 <body>
 
-<h1>Smooth Scroll</h1>
+<h1>Drinkkiohjeet</h1>
 
 <div class="main" id="section1">
-  <h2>Drinkkiohjeet</h2>
-  <p>Click on the link to see the "smooth" scrolling effect.</p>
-  <a href="#section2">Click Me to Smooth Scroll to Section 2 Below</a>
-  <p>Note: Remove the scroll-behavior property to remove smooth scrolling.</p>
+  <h2>Selaa ohjeita</h2>
+  <a href="#section2">Alas</a>
 </div>
 
 <div class="main" id="section2">
   <h2>Varalta toinen</h2>
-  <a href="#section1">Click Me to Smooth Scroll to Section 1 Above</a>
+  <a href="#section1">Takas ylös</a>
 </div>
 
 </body>
