40e0caa48e7f5634330c09cfee12ae2f70e77026
siiseli19 <sami.ojala97@gmail.com>
2021-05-09 22:35:26 +0300

    jj

--- Drinkkiohjeet.php
+++ Drinkkiohjeet.php
@@ -45,7 +45,7 @@ $(document).ready(function(){
 <h1>Smooth Scroll</h1>
 
 <div class="main" id="section1">
-  <h2>Section 1</h2>
+  <h2>Drinkkiohjeet</h2>
   <p>Click on the link to see the "smooth" scrolling effect.</p>
   <a href="#section2">Click Me to Smooth Scroll to Section 2 Below</a>
   <p>Note: Remove the scroll-behavior property to remove smooth scrolling.</p>
