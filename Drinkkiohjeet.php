6d2faf17abfe1fa8775a96d7e9fc82484a200116
siiseli19 <sami.ojala97@gmail.com>
2021-05-10 22:10:12 +0300

    uu

--- Drinkkiohjeet.php
+++ Drinkkiohjeet.php
@@ -139,7 +139,7 @@
 
 <div class="container">
   <div class="row">
-    <div class="col">Column</div>
+    <div class="col">Colu</div>
     <div class="col">Column</div>
     <div class="w-100"></div>
     <div class="col">Column</div>
