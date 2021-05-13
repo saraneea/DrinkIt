df84cd6b2f5d7494d19af37cfa20427530e65947
siiseli19 <58794075+siiseli19@users.noreply.github.com>
2021-05-13 11:32:18 +0300

    Create Luokka.php
    Luokka

---
+++ Luokka.php
@@ -0,0 +1,15 @@
+<?php
+class Drinkki {
+  // Properties
+  public $nimi;
+  public $ainesosa;
+
+  // Methods
+  function set_nimi($nimi) {
+    $this->nimi = $nimi;
+  }
+  function get_nimi() {
+    return $this->nimi;
+  }
+}
+?>
